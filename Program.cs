597b1e9555bace0d6f2d53feb5770b73f9be0b94
James Morcom <james@Jamess-MacBook-Pro.local>
2017-01-22 16:32:01 +0000

    Basic web app.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Hosting;
+
+namespace Hypertextor {
+  public class Program {
+    public static void Main(string[] args) {
+      
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hypertextor
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app){
+            app.Run(context => context.Response.WriteAsync("Hypertextor"));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
